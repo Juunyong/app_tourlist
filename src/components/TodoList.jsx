9dc2a066ab7f7296d4e04ccd11edcd180e7d0e3b
Juunyong <qkrwnsdyd212@gmail.com>
2024-11-26 16:32:16 +0900

    1126

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import TodoItem from './TodoItem';
 import { set } from 'date-fns';
 import { useTodo } from '@/contexts/TodoContext';
@@ -11,9 +11,33 @@ const TodoList = () => {
         return todos.filter((item) => item.task.toLowerCase().includes(search.toLowerCase()));
     };
 
+    const lookback = useMemo(() => {
+        console.log('lb');
+        const total = todos.length;
+        const done = todos.filter((todo) => todo.isDone).length;
+        const left = total - done;
+        return { total, done, left };
+    }, [todos]);
+
+    const handleUpdate = useCallback(
+        (id) => {
+            return onUpdate(id);
+        },
+        [onUpdate]
+    );
+    const handleDelete = useCallback(
+        (id) => {
+            return onDelete(id);
+        },
+        [onUpdate]
+    );
     return (
         <div>
             <h2>할 일 목록</h2>
+            <hr></hr>
+            <div>
+                {lookback.total}중 {lookback.done}완료. {lookback.left}개 남음.
+            </div>
             <input
                 type="search"
                 value={search}
@@ -27,7 +51,14 @@ const TodoList = () => {
                 {filteredTodos().map(
                     (item) => (
                         console.log(item),
-                        (<TodoItem key={item.id} {...item} onUpdate={onUpdate} onDelete={onDelete} />)
+                        (
+                            <TodoItem
+                                key={item.id}
+                                {...item}
+                                onUpdate={() => handleUpdate(item.id)}
+                                onDelete={() => handleDelete(item.id)}
+                            />
+                        )
                     )
                 )}
             </ul>
